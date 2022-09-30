30d335ee04c1220b62306fa603b53110bdfffced
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-09-30 19:40:25 +0100

    Added file contents packet type.

--- Code/Packets.py
+++ Code/Packets.py
@@ -1,4 +1,4 @@
-typeToNum = {"FileRequest": 0}
+typeToNum = {"FileRequest": 0, "FileContents": 1}
 
 class FileRequestPacket():
 	def __init__(self, filename):
@@ -19,8 +19,31 @@ class FileRequestPacket():
 		filename = packet[1:].decode("ascii")	
 		return cls(filename)
 
-numToClass = {0: FileRequestPacket}
+class FileContentsPacket():
+	def __init__(self, position, data):
+		self.type = typeToNum["FileContents"]
+		self.position = position
+		self.data = data
+	
+	def encode(self):
+		# Store type of packet in first byte
+		typeByte = (self.type).to_bytes(1, byteorder='big')
+		# Store position in second byte
+		positionByte = (self.position).to_bytes(1, byteorder='big')
+		return typeByte+positionByte+self.data
+
+	@classmethod
+	def decode(cls, packet):
+		# Position in second byte
+		position = packet[1]
+		# Rest is file data
+		data = packet[2:]
+		return cls(position, data)
+
+numToClass = {0: FileRequestPacket, 1: FileContentsPacket}
 
 def decodePacket(packet):
 	packetType = packet[0]
+	if packetType not in numToClass:
+		return None
 	return numToClass[packetType].decode(packet)
