c6c3fb94c6a0573390970b5b3c5e45dce062db5f
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-09-30 18:49:52 +0100

    Request packet gets from client to ingress to worker.

--- Code/Ingress.py
+++ Code/Ingress.py
@@ -6,7 +6,8 @@ LISTEN_PORT = 6000
 listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 listener.bind(("", LISTEN_PORT))
 
-availableWorkers = {"172.41.0.2", "172.17.0.3", "172.17.0.4"}
+#availableWorkers = {"172.41.0.2", "172.17.0.3", "172.17.0.4"}
+availableWorkers = {"172.41.0.2"}
 workerToClient = {}
 clientToWorker = {}
 
@@ -19,3 +20,11 @@ while True:
 
 	if packet.type == Packets.typeToNum["FileRequest"]:
 		print("Recieving request for {} from {}".format(packet.filename, addr[0]))
+
+		# Assign worker.
+		currentWorker = availableWorkers.pop()
+		workerToClient[currentWorker] = addr[0]
+		clientToWorker[addr[0]] = currentWorker
+
+		# Send request to worker.
+		listener.sendto(data, (currentWorker, LISTEN_PORT))
--- Code/Worker.py
+++ Code/Worker.py
@@ -1,12 +1,17 @@
 import socket
+import Packets
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+LISTEN_PORT = 6000
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+listener.bind(("", LISTEN_PORT))
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
+	data, addr = listener.recvfrom(512)
+
+	packet = Packets.decodePacket(data)
+	if packet == None:
+		continue
+
+	if packet.type == Packets.typeToNum["FileRequest"]:
+		print("Recieving request for {} from load balancer.".format(packet.filename, addr[0]))
