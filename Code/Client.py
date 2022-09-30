e76ec7150ba12747f86e65756f8c6069ae2f54b3
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-09-30 20:23:47 +0100

    Ingress forwards file contents from worker to client.

--- Code/Client.py
+++ Code/Client.py
@@ -3,6 +3,9 @@ import Packets
 
 LOADBALANCER_IP = "172.40.0.2"
 PORT = 6000
+
+PACKETS_PER_CHUNK = 32
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", PORT))
 
@@ -12,3 +15,15 @@ print("Sending request!")
 requestPacket = Packets.FileRequestPacket(filename)
 sock.sendto(requestPacket.encode(), (LOADBALANCER_IP, PORT))
 
+fileRecieved = False
+
+while not fileRecieved:
+	data, addr = sock.recvfrom(512)
+
+	packet = Packets.decodePacket(data)
+	if packet == None:
+		continue
+
+	if packet.type == Packets.typeToNum["FileContents"]:
+		print("Recieving file contents from ingress. Packet {} out of {} in chunk."
+			.format(packet.position, PACKETS_PER_CHUNK))
