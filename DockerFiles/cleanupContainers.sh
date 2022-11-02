d6748533f222989785aeeffeaf8bb36b3a4abd56
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-11-02 22:27:44 +0000

    Cleanup script stops containers.

--- DockerFiles/cleanupContainers.sh
+++ DockerFiles/cleanupContainers.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
+docker stop client
+docker stop loadbalancer
+docker stop worker1
+docker stop worker2
+docker stop worker3
+
 docker network remove externalNetwork
 docker network remove internalNetwork
 
