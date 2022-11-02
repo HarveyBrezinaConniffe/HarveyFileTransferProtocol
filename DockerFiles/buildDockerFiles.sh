7a0d8fbb5c0c4ef089fbde57d86a762b0526afb2
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-11-02 22:33:30 +0000

    Use entrypoint to start programs.

--- DockerFiles/buildDockerFiles.sh
+++ DockerFiles/buildDockerFiles.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
 tar -cvzf Code.tar.gz ../Code/
+tar -cvzf HostedFiles.tar.gz ../HostedFiles/
 cp Code.tar.gz Client/Code.tar.gz
 cp Code.tar.gz LoadBalancer/Code.tar.gz
 cp Code.tar.gz Worker/Code.tar.gz
+cp HostedFiles.tar.gz Worker/HostedFiles.tar.gz
 docker build -t clientimage ./Client
 docker build -t loadbalancerimage ./LoadBalancer
 docker build -t workerimage ./Worker
